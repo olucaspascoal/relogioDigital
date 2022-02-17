1165ece07eac964af60437192e714e15654e83b7
Lucas Pascoal <olucaspascoal@gmail.com>
2022-02-17 01:01:18 -0300

    Inclusão da data atual

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-var hora, min, sec;
+var hora, min, sec, dataCompleta;
+var diasSemana = ['Domingo', 'Segunda-feira', 'Terça-feria', 'Quarta-feria', 'Quinta-feira', 'Sexta-feria', 'Sábado'];
+var meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
 
 var horarioAtual = function(){
     var date = new Date(); 
@@ -10,6 +12,23 @@ var horarioAtual = function(){
     document.getElementById('time').innerHTML = horario;
 }
 
-setInterval( function(){
+var date = new Date()
+
+var diaAtual = (date) => {
+    numSemana = date.getDay()
+    return diasSemana[numSemana]
+}
+
+function mesAtual(date) {
+    var numMes = date.getMonth()
+    return meses[numMes];
+}
+
+dataCompleta = diaAtual(date) + ', ' + date.getDate() + ' de ' + mesAtual(date) + ' de ' + date.getFullYear();
+
+document.getElementById('data').innerHTML = dataCompleta;
+
+horarioAtual();
+setInterval( () => {
     horarioAtual()
 }, 1000);
